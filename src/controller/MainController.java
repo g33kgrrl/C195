c0680deaf089b12cd3d41940b5c0b98385bfd788
g33kgrrl <g33kgrrl@users.noreply.github.com>
2022-04-17 21:54:11 -0700

    User class, Submit button action

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -1,13 +1,26 @@
 package controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
+
+    public TextField userName;
+    public TextField password;
+    public Label zoneIdLabel;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         System.out.println("Hello students");
+        zoneIdLabel.setText("U.S.");
+    }
+
+    public void onSubmitButtonAction(ActionEvent actionEvent) {
+        System.out.println("Submit button clicked!");
     }
 }
---
+++ src/model/User.java
@@ -0,0 +1,80 @@
+package model;
+
+import java.time.LocalDateTime;
+
+public class User {
+    private int userId;
+    private String userName;
+    private String password;
+    private LocalDateTime createDate;
+    private String createdBy;
+    private LocalDateTime lastUpdate;
+    private String lastUpdatedBy;
+
+
+    public User(int userId, String userName, String password, LocalDateTime createDate, String createdBy, LocalDateTime lastUpdate, String lastUpdatedBy) {
+        this.userId = userId;
+        this.userName = userName;
+        this.password = password;
+        this.createDate = createDate;
+        this.createdBy = createdBy;
+        this.lastUpdate = lastUpdate;
+        this.lastUpdatedBy = lastUpdatedBy;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDateTime getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(LocalDateTime createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public LocalDateTime getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(LocalDateTime lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    public String getLastUpdatedBy() {
+        return lastUpdatedBy;
+    }
+
+    public void setLastUpdatedBy(String lastUpdatedBy) {
+        this.lastUpdatedBy = lastUpdatedBy;
+    }
+}
